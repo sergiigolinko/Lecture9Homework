1e39314045aadf18f23d08a7a30b95a29197dcde
sergii <sergii.glavmarket.com@gmail.com>
2019-12-01 17:44:25 +0200

    message3

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,21 +1,23 @@
+import java.util.Arrays;
+
 public class ArrayQueue implements IntQueue {
     private int[] values=new int[5];
-    private int firstInQueue;
-    private int lastInQueue;
+    private int firstInQueue=-1;
+    private int lastInQueue=-1;
 
 
     public void add(int i) {
-        if (++lastInQueue == values.length) {
-            lastInQueue = 0;
+        if (lastInQueue == values.length-1) {
+            values= Arrays.copyOf(values,values.length*2);
         }
-        values[lastInQueue] = i;
+        values[++lastInQueue] = i;
     }
 
     public int peek() {
-        if (++firstInQueue == values.length) {
+        if (firstInQueue == values.length) {
             firstInQueue = 0;
             return 0;
         }
-        return values[firstInQueue];
+        return values[++firstInQueue];
     }
 }
--- src/TestQueue.java
+++ src/TestQueue.java
@@ -1,8 +1,12 @@
 public class TestQueue {
     public static void main(String args[]){
 IntQueue queueArray=new ArrayQueue();
-queueArray.add(1);
+queueArray.add(6);
 queueArray.add(2);
+queueArray.add(3);
+queueArray.add(3);
+queueArray.add(3);
+queueArray.add(3);
 queueArray.add(3);
 System.out.println(queueArray.peek());
 IntQueue queueLink=new LinkedQueue();
